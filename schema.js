336f6461c34a779668252f1238c09d66386329f6
OtaiprO <159168309+OtaiprO@users.noreply.github.com>
2024-06-20 23:56:08 +0200

    Made the schemas more complex args, connected Queries, added Mutations
    Added update, delete, add game Mutations, Connected Reviews with game and authors ids, added inputs to use inside the Mutations

--- schema.js
+++ schema.js
@@ -4,23 +4,44 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platforms: [String!]!
+        reviews: [Review!]
     } 
 
     type Review {
         id: ID!
         rating: Int!,
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     type Query {
         reviews: [Review]
+        review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
+    }
+    
+    type Mutation {
+        addGame(game: AddGameInput!): Game
+        deleteGame(id: ID!): [Game]
+        updateGame(id: ID!, edits: EditGameInput): Game
+    }
+    input AddGameInput {
+        title: String!,
+        platforms: [String!]!
+    }
+    input EditGameInput {
+        title: String,
+        platforms: [String!]
     }
 `;
